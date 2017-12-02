33e4e25880016b7da9dd25cf8942d77199899371
IncredibLink <incrediblelink@gmail.com>
2017-12-02 11:22:17 -0500

    修复点击 AnchoredNews 会让卡片消失的 Bug...

--- const.js
+++ const.js
@@ -1,6 +1,6 @@
 module.exports = {
   baseUrl: 'https://langchao.co/',
-  api: 'https://a.langchao.co/',
+  api: 'http://localhost:3422/',
   static: 'https://s.langchao.co/',
   ga: 'UA-109441031-1' // Google Analytics
 }
