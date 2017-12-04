6dd021b0753eed75a8af019086759eec534d0767
IncredibLink <incrediblelink@gmail.com>
2017-12-04 13:06:19 -0500

    将只用过一次的 Element Component 移到 Component 内引用
    将 app.js 大小从 630kB 减到 459kB —— 郑多燕极限美形班火热招生中

--- plugins/element.js
+++ plugins/element.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Button from 'element-ui/lib/button'
-import DatePicker from 'element-ui/lib/date-picker'
-import TimeSelect from 'element-ui/lib/time-select'
-import TimePicker from 'element-ui/lib/time-picker'
 import Tooltip from 'element-ui/lib/tooltip'
 import Input from 'element-ui/lib/input'
 import Select from 'element-ui/lib/select'
@@ -10,18 +7,10 @@ import Option from 'element-ui/lib/option'
 import Form from 'element-ui/lib/form'
 import FormItem from 'element-ui/lib/form-item'
 import Icon from 'element-ui/lib/icon'
-import Step from 'element-ui/lib/step'
-import Steps from 'element-ui/lib/steps'
 import Message from 'element-ui/lib/message'
-import Upload from 'element-ui/lib/upload'
-import Checkbox from 'element-ui/lib/checkbox'
-import CheckboxGroup from 'element-ui/lib/checkbox-group'
 import Popover from 'element-ui/lib/popover'
 
 Vue.use(Button)
-Vue.use(DatePicker)
-Vue.use(TimeSelect)
-Vue.use(TimePicker)
 Vue.use(Tooltip)
 Vue.use(Input)
 Vue.use(Select)
@@ -29,11 +18,6 @@ Vue.use(Option)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Icon)
-Vue.use(Step)
-Vue.use(Steps)
-Vue.use(Upload)
-Vue.use(Checkbox)
-Vue.use(CheckboxGroup)
 Vue.use(Popover)
 
 Vue.prototype.$message = Message
