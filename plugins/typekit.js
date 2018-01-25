f12498b3e45bc3ae46d2f18be3308d694dbd48b1
IncredibLink <incrediblelink@gmail.com>
2018-01-25 17:52:05 -0500

    修复两个智障错误

--- plugins/typekit.js
+++ plugins/typekit.js
@@ -3,10 +3,9 @@
 export default () => {
   (function (d) {
     var config = {
-      kitId: 'zmq2epg',
       scriptTimeout: 3000,
       async: true
     },
-      h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'http://localhost:3000/typekit.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
+      h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://static.v2land.net/typekit.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
   })(document)
 }
