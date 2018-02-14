f3bdf9ae1b692030062095e9ecf5834fe23938e4
IncredibLink <simon.zehua.li@gmail.com>
2018-02-13 23:05:14 -0500

    改正错误的 commit

--- const.js
+++ const.js
@@ -1,6 +1,6 @@
 module.exports = {
-  baseUrl: 'http://localhost:3000/',
-  api: 'http://localhost:1337/',
+  baseUrl: 'https://langchao.co/',
+  api: 'https://a.langchao.co/',
   static: 'https://assets.v2land.net/',
   ga: 'UA-109441031-1', // Google Analytics
   typekit: 'zmq2epg',
