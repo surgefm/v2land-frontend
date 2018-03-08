9c81da401d52823a9deb8a837634e84ee1b0d29e
IncredibLink <simon.zehua.li@gmail.com>
2018-03-08 00:37:11 -0500

    修改文案

--- components/EventAction/EventActionEdit.vue
+++ components/EventAction/EventActionEdit.vue
@@ -3,7 +3,7 @@
     class="subscribe-container"
     :to="'/' + this.$route.params.name + '/edit'"
   >
-    <span class="text light-font">修改事件</span>
+    <span class="text light-font">管理事件</span>
     <i class="icon el-icon-edit-outline light-font" />
   </nuxt-link>
 </template>
