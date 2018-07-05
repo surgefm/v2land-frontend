ae493a35b74f567d6a5993fa7ef31e800b3b4831
Link <incrediblelink@gmail.com>
2018-07-05 01:53:48 -0400

    增加管理进展新闻的功能

--- components/EventStack/EventStackNewsArrange.vue
+++ components/EventStack/EventStackNewsArrange.vue
@@ -1,32 +1,66 @@
 <template>
-  <div>
-    <el-form class="form" ref="form" label-width="72px">
-      <el-form-item label="新闻状态">
-        <el-checkbox-group v-model="filterStatus" class="align-center">
-          <el-checkbox
-            v-for="item of status"
-            :key="item.label"
-            :label="item.label"
-          >{{ item.text }}</el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="搜索标题">
-        <el-input v-model="filterTitle" placeholder="搜索新闻标题" />
-      </el-form-item>
-      <el-form-item label="搜索来源">
-        <el-input v-model="filterSource" placeholder="搜索新闻来源" />
-      </el-form-item>
-    </el-form>
+<div>
+  <el-form class="form" ref="form" label-width="80px">
+    <el-form-item label="新闻状态">
+      <el-checkbox-group v-model="filterStatus" class="align-center">
+        <el-checkbox
+          v-for="item of status"
+          :key="item.label"
+          :label="item.label"
+        >{{ item.text }}</el-checkbox>
+      </el-checkbox-group>
+    </el-form-item>
+    <el-form-item label="搜索标题">
+      <el-input v-model="filterTitle" placeholder="搜索新闻标题" />
+    </el-form-item>
+    <el-form-item label="搜索来源">
+      <el-input v-model="filterSource" placeholder="搜索新闻来源" />
+    </el-form-item>
+  </el-form>
+  <div class="create-news">
+    <el-button
+      type="primary"
+      size="medium"
+      @click="beginNewsEditing('create')"
+    >
+      添加新闻
+    </el-button>
+  </div>
+
+  <div class="news-list">
     <event-stack-news
       v-for="news of newsList"
       mode="edit"
+      :news="news"
       :key="news.id"
+      @edit="beginNewsEditing('edit')"
     />
+    <div class="empty-news-list" v-if="newsList.length === 0">
+      <span>未找到相关新闻</span>
+    </div>
   </div>
+
+  <el-dialog
+    :title="newsEditMode === 'edit' ? '编辑新闻' : '添加新闻'"
+    :visible.sync="dialogVisible"
+    :append-to-body="true"
+    @close="finishNewsEditing()"
+  >
+    <event-news-information-form
+      :mode="newsEditMode"
+      :data="newsEditMode === 'edit' ? 'updateStackNews' : 'createStackNews'"
+      :news="newsEditData"
+      :stack="stack"
+      v-on:submit="submit"
+      ref="newsForm"
+    />
+  </el-dialog>
+</div>
 </template>
 
 <script>
 import EventStackNews from '~/components/EventStack/EventStackNews.vue';
+import EventNewsInformationForm from '~/components/EventNews/EventNewsInformationForm.vue';
 import getSortedId from '~/utils/getSortedId.js';
 import Checkbox from 'element-ui/lib/checkbox';
 import CheckboxGroup from 'element-ui/lib/checkbox-group';
@@ -52,10 +86,13 @@ export default {
         { label: 'rejected', text: '拒绝' },
         { label: 'removed', text: '移除' },
       ],
+      newsEditMode: 'create',
+      newsEditData: {},
+      dialogVisible: false,
     };
   },
   methods: {
-    async update() {
+    update() {
       this.filterChange = true;
       if (this.updateTimer) {
         clearTimeout(this.updateTimer);
@@ -84,16 +121,61 @@ export default {
         page: this.page,
       });
 
-      if (!this.filterChange) {
-        this.newsList = newsList;
-      } else {
+      if (this.filterChange) {
+        this.newsList = newsList.slice();
         this.filterChange = false;
-        this.newsList = getSortedId({
+      } else {
+        const sorted = getSortedId({
           collection: this.newsList.concat(newsList),
           isDecr: false,
           returnId: false,
         });
+        for (let i = sorted.length - 1; i > 0; i--) {
+          if (sorted[i].id == sorted[i - 1].id) {
+            sorted.splice(i, 1);
+          }
+        }
+        this.newsList = sorted;
       }
+    },
+    async submit() {
+      try {
+        if (this.newsEditMode === 'create') {
+          const data = this.$store.state.temp.createStackNews;
+          const url = `event/${this.stack.event}/news`;
+          await this.$axios.post(url, data);
+          this.$message.success('提交成功');
+        } else if (this.newsEditMode === 'edit') {
+          const data = this.$store.state.temp.updateStackNews;
+          await this.$store.dispatch('editNews', {
+            id: data.id,
+            data,
+          });
+          this.$message.success('修改成功');
+        }
+        this.finishNewsEditing();
+      } catch (err) {
+        console.error(err);
+        this.$message.error('提交过程中发生了错误');
+      } finally {
+        this.$refs.newsForm.resetButton();
+      }
+    },
+    beginNewsEditing(mode = 'create') {
+      this.newsEditMode = mode;
+      this.newsEditData = {};
+      if (mode === 'edit') {
+        this.newsEditData = { ...this.$store.state.temp.newsToEdit };
+      }
+      this.dialogVisible = true;
+      this.$store.commit('setTemp', {
+        label: 'newsToEdit',
+        temp: null,
+      });
+    },
+    finishNewsEditing() {
+      this.dialogVisible = false;
+      this.update();
     },
   },
   async created() {
@@ -112,6 +194,7 @@ export default {
   },
   components: {
     'event-stack-news': EventStackNews,
+    'event-news-information-form': EventNewsInformationForm,
     'el-checkbox': Checkbox,
     'el-checkbox-group': CheckboxGroup,
   },
@@ -119,5 +202,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.align-center {
+  line-height: 40px;
+}
 
+.create-news {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.news-list {
+  margin-top: 12px;
+}
+
+.empty-news-list {
+  display: flex;
+  justify-content: center;
+}
+
+.empty-news-list span {
+  user-select: none;
+  padding: .4rem 1rem;
+  background-color: #eee;
+  border-radius: .25rem;
+}
 </style>
