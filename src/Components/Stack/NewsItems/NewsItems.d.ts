010f0bb4d72492a5413ecd1d36744a987c2c7b9e
Kefli <likefan2009@sina.com>
2020-04-20 01:34:11 -0400

    Revert "重新设计和实现事件界面，调整Card的宽度，增加NewsItem和NewsIItems两个新的component"

--- src/Components/Stack/NewsItems/NewsItems.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace INewsItems {
-  export interface IProps {
-    newsIdList: number[];
-  }
-}
-
-export { INewsItems };
--- src/Components/Stack/index.tsx
+++ src/Components/Stack/index.tsx
@@ -1,115 +1,59 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { getStack, getStackNewsIdList, getNews } from '@Selectors';
-import { Card } from '@Components/Basic';
-import { IStack } from './Stack';
-import { NewsItems } from './NewsItems';
 
+import { getStack } from '@Selectors';
+import { Card } from '@Components/Basic';
+
+import { IStack } from './Stack';
 
 const Stack: React.FunctionComponent<IStack.IProps> = ({ stackId }) => {
   const stack = useSelector(getStack(stackId));
   if (!stack) return <div />;
 
-  const newsIdList = useSelector(getStackNewsIdList(stackId));
-
   return (
-    <Card styles={{ paddingTop: '1px', overflow: 'hidden', paddingBottom: '0' }}>
+    <Card>
       <div className="stack">
-        <div className="stack-main">
-          <div className="title-area">
-            {stack.order ? (
-              <span className="order-redesigned">
-                <p>{stack.order}</p>
-              </span>
-            ) : null}
-            <h2>{stack.title}</h2>
-          </div>
+        {stack.order ? <span className="order light-font">{stack.order}</span> : null}
+        <span>{stack.title}</span>
+      </div>
+      <style jsx>
+        {`
+          .stack {
+            position: relative;
+            padding: 0;
+          }
 
-          <div className="content-area">
-            <p>{stack.description}</p>
-          </div>
-        </div>
+          .order {
+            font-size: 4.25rem;
+            height: 4.25rem;
+            line-height: 1;
+            position: absolute;
+            right: calc(100% - 1.25rem);
+            top: 0.5rem;
+            font-family: 'Times New Roman', Times, serif;
+            cursor: pointer;
+            font-weight: 900;
+            transition: all 0.2s;
+          }
 
-        <hr />
-
-        <NewsItems newsIdList={newsIdList} />
-
-        <style jsx>
-          {`
-            @import url('https://fonts.googleapis.com/css?family=Lexend+Giga&display=swap');
-
-            .stack-main {
-              padding-bottom: 0.3rem;
-            }
-
-            .title-area {
-              display: flex;
-            }
-
-            .title-area h2 {
-              padding-top: 10px;
-              margin-left: 1rem;
-              line-height: 1.5;
-            }
-
-            .order-redesigned p {
-              margin-top: 0;
-              margin-bottom: -2.5rem;
-              font-family: 'Lexend Giga', sans-serif;
-              font-size: 3rem;
-              color: rgba(104, 180, 252);
-            }
-
-            .content-area {
-              margin-top: -1.3rem;
-            }
-
-            .content-area p {
-              display: block;
-              margin-left: 5.5px;
-              margin-right: 5.5px;
-            }
-
-            hr {
-              border-top: 1px dashed #8c8b8b;
+          @media (max-width: 600px) {
+            .stack {
+              padding: 0.75rem 1rem;
             }
 
             .order {
-              color: white;
-              background-color: black;
-              height: 5rem;
-              width: 5rem;
-              font-size: 4.25rem;
-              //height: 4.25rem;
-              //line-height: 1;
               position: relative;
-              //right: calc(100% - 1.25rem);
-              //top: 0.5rem;
-              font-family: 'Times New Roman', Times, serif;
-              cursor: pointer;
-              font-weight: 900;
-              transition: all 0.2s;
+              right: initial;
+              top: 0;
+              left: -0.25rem;
+              height: 3rem;
+              font-size: 3.5rem;
+              float: left;
+              text-shadow: none !important;
             }
-
-            @media (max-width: 600px) {
-              .stack {
-                padding: 0.75rem 1rem;
-              }
-
-              .order {
-                position: relative;
-                right: initial;
-                top: 0;
-                left: -0.25rem;
-                height: 3rem;
-                font-size: 3.5rem;
-                float: left;
-                text-shadow: none !important;
-              }
-            }
-          `}
-        </style>
-      </div>
+          }
+        `}
+      </style>
     </Card>
   );
 };
